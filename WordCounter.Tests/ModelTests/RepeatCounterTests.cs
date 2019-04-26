9bd86aa6ae93569d04b808f7806f90aafa618a5e
sabrina mianne <sabrina.epicodus@gmail.com>
2019-04-26 16:42:40 -0700

    Add method to convert string in lower case and test it succesfully

--- WordCounter.Tests/ModelTests/RepeatCounterTests.cs
+++ WordCounter.Tests/ModelTests/RepeatCounterTests.cs
@@ -47,10 +47,28 @@ namespace WordCounter.Tests
             bool resultCheckBool2 = Check.CheckBoolOccurence (userSentence,word2);
             //Assert
             Assert.AreEqual(true, resultCheckBool);
-            Assert.AreEqual(false, resultCheckBool2);
-          
-            
+            Assert.AreEqual(false, resultCheckBool2);        
         }
 
+        [TestMethod]
+        public void CheckNumberOccurence_GetCountForOccurrence_Int()
+        {
+            //Arrange
+            string word = "car";
+            string word2 ="card";
+            string wordUpperCase = "CAR";
+            string sentence = "I need to buy a new car, a blue car.";
+            RepeatCounter userInput = new RepeatCounter(word,sentence);
+            //Act
+            string userWord = userInput.GetUserWord();
+            string userSentence= userInput.GetUserSentence();
+            int resultCheckCount = Check.CheckNumberOccurence (userSentence,userWord);
+            int resultCheckCount2 = Check.CheckNumberOccurence(userSentence,word2);
+            int resultCheckCountUpperCase = Check.CheckNumberOccurence(userSentence,wordUpperCase);
+            //Assert
+            Assert.AreEqual(2, resultCheckCount);
+            Assert.AreEqual(0, resultCheckCount2);        
+            Assert.AreEqual(2, resultCheckCountUpperCase);  
+        }
     }
 }
---
+++ WordCounter/Models/RepeatCounter.cs
@@ -0,0 +1,52 @@
+namespace WordCounter.Models
+{
+    public class RepeatCounter
+    {
+        private string _word;
+        private string _sentence;
+
+        public RepeatCounter (string word, string sentence)
+        {
+            _word = word;
+            _sentence = sentence;
+        }
+
+        public string GetUserWord()
+        {
+            return _word;
+        }
+
+         public string GetUserSentence()
+        {
+            return _sentence;
+        }
+    }
+    public static class Check
+    {
+        public static bool CheckBoolOccurence (string sentence, string word)
+        {
+            if (sentence.IndexOf(word) != -1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static int CheckNumberOccurence(string sentence, string word)
+        {
+            int countOccurence = 0;
+            int indexOccur = 0;
+             word = word.ToLower();
+             sentence = sentence.ToLower();
+            while ((indexOccur = sentence.IndexOf(word,indexOccur)) != -1)
+            {
+                indexOccur += word.Length;
+                countOccurence += 1;
+            }
+                return countOccurence;           
+        }
+    }
+}
